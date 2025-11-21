24a4279d6b71637f90e88077d88a07f1e00459fa
aishwarya <ashp17star@gmail.com>
2025-11-21 15:45:05 +0530

    .

--- Swift_Basics_Rev_3.playground/Contents.swift
+++ Swift_Basics_Rev_3.playground/Contents.swift
@@ -37,3 +37,51 @@ if let name2 = name1{
 }else{
     print("name is nil")
 }
+
+//optional binding -- guard let
+var instituteName1 : String? = nil
+func checkForValue(instituteName : String?){
+    guard let name = instituteName else{
+        print("the institute name is nil")
+        return
+    }
+    
+    print("The institute name is : \(name)")
+}
+
+checkForValue(instituteName: instituteName1)
+
+
+for i in 1...10{
+    print(i * 5)
+}
+
+for i in stride(from: 10, to: 20, by: 2){
+    print(i)
+}
+
+for i in stride(from: 10, through: 50, by: 5){
+    print(i)
+}
+
+//function definition
+func addition(numberOne : Int, numberTwo : Int){
+    print(numberOne + numberTwo)
+}
+
+addition(numberOne: 12, numberTwo: 13)          //function call
+var n0 = 13
+var n7 = 223
+addition(numberOne: n0, numberTwo: n7)
+
+var arr1 = [23,49,90,34,2,1,4,87]
+func sumOfElements(arr : [Int]) -> Int{
+    var sum = 0
+    for i in arr{
+        sum = sum + i
+    }
+    return sum
+}
+
+var result4 = sumOfElements(arr: arr1)
+print(result4)
